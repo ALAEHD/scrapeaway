7e4750589481c32341ce2fbbb853e1a3fcd568ef
Alae El kandoussi <elkandoussialae@gmail.com>
2024-06-05 20:07:29 +0100

    Bind to PORT environment variable

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, flash, redirect
 from scraper import (
     scrape_jumia, scrape_electroplanet, scrape_virgin,
@@ -92,4 +93,5 @@ def send_email():
     return redirect('/')
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
