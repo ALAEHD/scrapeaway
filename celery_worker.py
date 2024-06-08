986dde76372557377acedd6129653088f847a461
Alae El kandoussi <elkandoussialae@gmail.com>
2024-06-08 12:12:21 +0100

    added authentification to celery_worker

--- celery_worker.py
+++ celery_worker.py
@@ -1,6 +1,6 @@
 from celery import Celery
 
-app = Celery('celery_worker', broker='redis://redis-11521.c3.eu-west-1-1.ec2.redns.redis-cloud.com:11521')
+app = Celery('celery_worker', broker='redis://default:oEjZ0wQcLbX5SbR4C2A6pfqpj57b7eas@redis-11521.c3.eu-west-1-1.ec2.redns.redis-cloud.com:11521')
 
 if __name__ == '__main__':
     app.start()
